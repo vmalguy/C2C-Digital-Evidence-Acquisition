1e6d02a90e48db3528cf26ae655b9c56117d8bd7
Vincent Malguy <vmalguy@users.noreply.github.com>
2020-12-09 11:10:15 -0600

    Update gen_temp_url.sh

--- gen_temp_url.sh
+++ gen_temp_url.sh
@@ -1,12 +1,34 @@
-# need source openrc.sh
-# apt install python-swiftclient wget
+# need openrc.sh and .rclone.conf
+#apt install python-swiftclient curl
 #curl https://rclone.org/install.sh | sudo bash
 
-PROVIDER="storage.us-east-va.cloud.ovh.us"
-AUTH="ENTER_YOUR_AUTH_URL"
+echo -n Dependancy verification :
+command -v rclone >/dev/null 2>&1 || ( apt install curl && curl -s https://rclone.org/install.sh | sudo bash )
+command -v swift >/dev/null 2>&1 || ( apt install python-swiftclient )
+echo Dependancy meet!
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied, need to be a container name, exemple : 1697438492"
+    exit 1
+fi
+
+# Setup rclone base on openstack configuration
+export RCLONE_CONFIG_MYREMOTE_TYPE=swift
+export RCLONE_CONFIG_MYREMOTE_ENV_AUTH=true
+mkdir -p ~/.config/rclone/ && touch ~/.config/rclone/rclone.conf
+SNAME=myremote
+SWIFT=${SNAME}:${CONTAINER}
+
+
+#provide URL key
+URLKEY=`swift stat|grep Temp-Url-Key|cut -d" " -f13`
+#provide OS_STORAGE_URL
+`swift  auth`
 
 for file in `swift list $1`
   do
-    URI=`swift tempurl GET 604800 /v1/${AUTH}/$1/$file  MYKEY`
-    echo wget --content-disposition -nc \"https://${PROVIDER}${URI}\"
+    URL=`swift tempurl GET 604800 ${OS_STORAGE_URL}/$1/$file  ${URLKEY}`
+    echo wget --content-disposition -nc \"${URL}\"
   done
+
